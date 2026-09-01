3ec48e3c710be5447946a4297722c1028707b704
olaru <olaru@rospace.com>
2026-09-01 15:52:59 +0300

    updates asset path handling

--- index.php
+++ index.php
@@ -10,13 +10,6 @@ const PAGE_REDIRECTS = [
     '/page/flow-libraries-email'   => 'https://www.dotkernel.com/#components-dot-packages',
 ];
 
-const ASSET_PATH_PREFIXES = [
-    '/css/',
-    '/js/',
-    '/fonts/',
-    '/images/',
-];
-
 const CATCH_ALL_REDIRECT = 'https://www.dotkernel.com/api/';
 
 $path = parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH) ?: '/';
@@ -25,15 +18,6 @@ $path = rtrim($path, '/') ?: '/';
 if (array_key_exists($path, PAGE_REDIRECTS)) {
     header('Location: ' . PAGE_REDIRECTS[$path], true, 301);
     exit;
-}
-
-foreach (ASSET_PATH_PREFIXES as $prefix) {
-    if (str_starts_with($path, $prefix)) {
-        http_response_code(410);
-        header('Content-Type: text/plain; charset=utf-8');
-        echo 'Gone';
-        exit;
-    }
 }
 
 header('Location: ' . CATCH_ALL_REDIRECT, true, 301);
