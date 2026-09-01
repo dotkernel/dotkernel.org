416f1568e3499d86676a9fb9b880a790d5dedb06
olaru <olaru@rospace.com>
2026-09-01 15:48:20 +0300

    edit htaccess

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 
 const PAGE_REDIRECTS = [
-    '/'                            => 'https://www.dotkernel.com/',
+    '/'                            => 'https://www.dotkernel.com/api',
     '/page/flow-middleware'        => 'https://www.dotkernel.com/#request-lifecycle',
     '/page/flow-libraries'         => 'https://www.dotkernel.com/#components-dot-packages',
     '/page/flow-libraries-email'   => 'https://www.dotkernel.com/#components-dot-packages',
